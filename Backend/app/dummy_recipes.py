497f16494f9bd0dcae4f5df09284aec8c8b7059f
Will Armstrong Sr <will.ea.armstrong@gmail.com>
2023-04-26 20:10:31 -0500

    added dummy and scratch

---
+++ Backend/app/dummy_recipes.py
@@ -0,0 +1,93 @@
+dummy_recipes = [
+    {
+        "id": "640803",
+        "title": "Crispy Buttermilk Fried Chicken",
+        "image": "https://spoonacular.com/recipeImages/640803-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/crispy-buttermilk-fried-chicken-640803"
+    },
+    {
+        "id": "633314",
+        "title": "Bacon Ice Cream",
+        "image": "https://spoonacular.com/recipeImages/633314-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Bacon-Ice-Cream-633314"
+    },
+    {
+        "id": "665734",
+        "title": "Zucchini Chicken Omelette",
+        "image": "https://spoonacular.com/recipeImages/665734-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Zucchini-Chicken-Omelette-665734"
+    },
+    {
+        "id": "650755",
+        "title": "Mango Black Tea Skinny Cocktail",
+        "image": "https://spoonacular.com/recipeImages/650755-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Mango-Black-Tea-Skinny-Cocktail-650755"
+    },
+    {
+        "id": "640370",
+        "title": "Cranberry Fettuccine Tomato Cream Sauce With Shrimps and Scallops",
+        "image": "https://spoonacular.com/recipeImages/640370-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Cranberry-Fettuccine-Tomato-Cream-Sauce-With-Shrimps-and-Scallops-640370"
+    },
+    {
+        "id": "633338",
+        "title": "Bacon Wrapped Filet Mignon",
+        "image": "https://spoonacular.com/recipeImages/633338-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Bacon-Wrapped-Filet-Mignon-633338"
+    },
+    {
+        "id": "658126",
+        "title": "Red Wine Stewed Oxtail",
+        "image": "https://spoonacular.com/recipeImages/658126-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Red-Wine-Stewed-Oxtail-658126"
+    },
+    {
+        "id": "641717",
+        "title": "Duck Rumaki",
+        "image": "https://spoonacular.com/recipeImages/641717-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Duck-Rumaki-641717"
+    },
+    {
+        "id": "646424",
+        "title": "Healthier Southern-Style Green Beans",
+        "image": "https://spoonacular.com/recipeImages/646424-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Healthier-Southern-Style-Green-Beans-646424"
+    },
+    {
+        "id": "649381",
+        "title": "Lazy Cobbler",
+        "image": "https://spoonacular.com/recipeImages/649381-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Lazy-Cobbler-649381"
+    },
+    {
+        "id": "649722",
+        "title": "Lemon Pepper Steak",
+        "image": "https://spoonacular.com/recipeImages/649722-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Lemon-Pepper-Steak-649722"
+    },
+    {
+        "id": "646216",
+        "title": "Ham With Bourbon, Molasses, and Pecan Glaze",
+        "image": "https://spoonacular.com/recipeImages/646216-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Ham-With-Bourbon,-Molasses,-and-Pecan-Glaze-646216"
+    },
+    {
+        "id": "1747693",
+        "title": "Your Basic Low Carb Breakfast",
+        "image": "https://spoonacular.com/recipeImages/1747693-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Your-Basic-Low-Carb-Breakfast-1747693"
+    },
+    {
+        "id": "715574",
+        "title": "Homemade Strawberry Shake",
+        "image": "https://spoonacular.com/recipeImages/715574-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Homemade-Strawberry-Shake-715574"
+    },
+    {
+        "id": "649495",
+        "title": "Lemon and Garlic Slow Roasted Chicken",
+        "image": "https://spoonacular.com/recipeImages/649495-312x231.jpg",
+        "link": "https://spoonacular.com/recipes/Lemon-and-Garlic-Slow-Roasted-Chicken-649495"
+    },
+    
+]
